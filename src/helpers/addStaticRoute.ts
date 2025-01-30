ac014dde85b100b587c3002b56d59cb3474e0f6e
LuiggiVal08 <luicciano.08.angel.2002@gmail.com>
2025-01-30 15:57:37 -0400

    fix: update static route to use process.cwd for correct path resolution

--- src/helpers/addStaticRoute.ts
+++ src/helpers/addStaticRoute.ts
@@ -2,5 +2,5 @@ import express, { type Application } from 'express';
 import path from 'path';
 
 export function addStaticRoute(app: Application, carpeta: string, ruta = `/${carpeta}`): void {
-    app.use(ruta, express.static(path.join(__dirname, '../../public', carpeta)));
+    app.use(ruta, express.static(path.join(process.cwd(), 'public', carpeta)));
 }
