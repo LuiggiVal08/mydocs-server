a372796680317a2f5fba3d68fef652d64d8e8745
LuiggiVal08 <luicciano.08.angel.2002@gmail.com>
2025-02-02 16:33:04 -0400

    docs: add JSDoc comments to handdleErrorsController function

--- src/helpers/handdleErrorsController.ts
+++ src/helpers/handdleErrorsController.ts
@@ -1,6 +1,12 @@
 import { Request, Response } from 'express';
 import { Error } from 'sequelize';
 
+/**
+ * Handles errors in controllers.
+ * @param error The error to be handled.
+ * @param res The response object.
+ * @param req The request object.
+ */
 const handdleErrorsController = (error: any, res: Response, req: Request): void => {
     const method = req.method;
     const action: 'crear' | 'eliminar' | 'editar' | 'obtener' =
